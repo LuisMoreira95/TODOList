fc08ea6aca31ee0d5913ac253ccd8807ebf362b5
llmoreira <llmoreira@corporativo.pt>
2023-06-18 01:40:17 +0100

    Implemented a repository pattern to enhance database changeability

--- TODOList.API/Program.cs
+++ TODOList.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TODOList.API.Data;
+using TODOList.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddSwaggerGen();
 // Ao adicionarmos a nossa connectionString aqui, o contexto da BD passa a estar disponível no projecto
 builder.Services.AddDbContext<TodoListDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("TodoListConnectionString")));
+
+builder.Services.AddScoped<ITodoRepository, SQLTodoRepository>();
 
 var app = builder.Build();
 
---
+++ TODOList.API/Repositories/SQLTodoRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TODOList.API.Data;
+using TODOList.API.Models.Domain;
+using TODOList.API.Models.DTO;
+
+namespace TODOList.API.Repositories
+{
+    public class SQLTodoRepository : ITodoRepository
+    {
+        private readonly TodoListDbContext dbContext;
+
+        public SQLTodoRepository(TodoListDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Todo>> GetAllAsync()
+        {
+            return await dbContext.Todos.ToListAsync();
+        }
+
+        public async Task<Todo?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Todos.FirstOrDefaultAsync(todo => todo.Id == id);
+        }
+        
+        public async Task<Todo> CreateAsync(Todo todo)
+        {
+            await dbContext.Todos.AddAsync(todo);
+            await dbContext.SaveChangesAsync();
+            return todo;
+        }
+        
+        public async Task<Todo?> UpdateAsync(Guid id, Todo todo)
+        {
+            var existingTodo = await dbContext.Todos.FirstOrDefaultAsync(todo => todo.Id == id);
+            if (existingTodo == null) 
+            {
+                return null;
+            }
+
+            existingTodo.Name = todo.Name;
+            existingTodo.Description = todo.Description;
+            existingTodo.CategoryId = todo.CategoryId;
+            existingTodo.CategoryName = todo.CategoryName;
+
+            await dbContext.SaveChangesAsync();
+            
+            return existingTodo;
+
+        }
+
+        public async Task<Todo?> DeleteAsync(Guid id)
+        {
+            var existingTodo = await dbContext.Todos.FirstOrDefaultAsync(todo => todo.Id == id);
+
+            if(existingTodo == null)
+            {
+                return null;
+            }
+
+            dbContext.Todos.Remove(existingTodo);
+            await dbContext.SaveChangesAsync();
+
+            return existingTodo;
+        }
+    }
+}
