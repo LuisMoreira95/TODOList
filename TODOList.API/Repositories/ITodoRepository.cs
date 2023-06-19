915e243a4db8f719dcca52dc459f980c35bd1fd7
llmoreira <llmoreira@corporativo.pt>
2023-06-19 21:26:55 +0100

    Added Filtering, Sorting and Pagination

--- TODOList.API/Repositories/ITodoRepository.cs
+++ TODOList.API/Repositories/ITodoRepository.cs
@@ -5,7 +5,9 @@ namespace TODOList.API.Repositories
     // Repositories Always Deal With Domain Models
     public interface ITodoRepository
     {
-        Task<List<Todo>> GetAllAsync();
+        Task<List<Todo>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+            string? sortBy = null, bool isAscending = true,
+            int pageNumber = 1, int pageSize = 1000);
         Task<Todo?> GetByIdAsync(Guid id);
         Task<Todo> CreateAsync(Todo todo);
         Task<Todo?> UpdateAsync(Guid id, Todo todo);
